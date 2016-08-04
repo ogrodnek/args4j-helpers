4bb13a83f571754741070a405fd5953e6a8b2aff
Larry Ogrodnek <ogrodnek@gmail.com>
2016-08-03 21:09:54 -0700

    trying to extract the init-sbt

--- build.sh
+++ build.sh
@@ -2,18 +2,6 @@
 
 set -x
 
-SBT=`mktemp /tmp/sbt.sh.XXXX`
+. `dirname $0`/init-sbt
 
-curl -L https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.12/sbt-launch.jar -o /tmp/sbt-launch.jar
-
-cat << "EOF" > $SBT
-#!/bin/bash
-
-SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-java $SBT_OPTS -jar `dirname $0`/sbt-launch.jar "$@"
-
-EOF
-
-chmod u+x $SBT
-
-$SBT compile
+sbt compile
